44aef4e0d1ffcf46d3c6793c653ba48620febad9
Felipe Silva <felipe_96sp@msn.com>
2019-10-13 20:33:56 -0300

    Cria conexão com o mongoDB Atlas.
    Realizando instalação do pacote do mongoose para criar conexão com o
    MongoDB Atlas.

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
+
+// Iniciando o DB
+mongoose.connect(
+  'mongodb+srv://starter:starter@cluster0-zmpmk.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
