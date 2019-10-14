a4dc053b9800a5fc4fb5eb6bbee0bd96035c2061
Felipe Silva <felipe_96sp@msn.com>
2019-10-13 21:03:28 -0300

    Cria um Product no banco
    Realiza a criação de um product teste no banco de dados.

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const app = express();
 
 // Iniciando o DB
 mongoose.connect(
-  'mongodb+srv://starter:starter@cluster0-zmpmk.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://starter:starter@cluster0-zmpmk.mongodb.net/starter?retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
+require('./src/models/Product');
+
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  });
   res.send('Hello World');
 });
 
